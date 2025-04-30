7f8709f2ecc447a2c329d73667fedd7eee5e783c
bniepce <brad.niepce@gmail.com>
2025-04-30 19:13:35 +0200

    Fix h5 append

--- src/converters/vindr.py
+++ src/converters/vindr.py
@@ -12,41 +12,82 @@ from src.operations.read import read_dicom
 from src.operations.normalize import normalize_int8
 from src.operations.transform import resize_square
 from math import ceil
+from contextlib import contextmanager
 
+class HDF5Store(object):
+    def __init__(self, datapath, datasets, shapes, dtype, compression="gzip", chunk_len=1):
+        self.datapath = datapath
+        self.datasets = datasets
+        self.shapes  = shapes
+        self.i = {dataset: 0 for dataset in datasets}
 
-def process_dicom_image(path: str) -> Optional[Tuple[str, np.ndarray]]:
-    try:
-        image = resize_square(normalize_int8(read_dicom(path)), new_size=256)
-        return path, image
-    except Exception as e:
-        logging.warning(f"Failed to process {path}: {e}")
-        return None
+        with h5py.File(self.datapath, mode='w') as h5f:
+            for idx, i in enumerate(datasets):
+                if shapes[idx] == 0:
+                    shape = (0,)
+                else:
+                    shape = (0,) + shapes[idx]
 
+                dtype_ = dtype[idx]
+
+                initial_maxshape = (None,) + shapes[idx] if len(shapes[idx]) != 0 else (None,)
+                initial_chunkshape = (chunk_len,) + shapes[idx] if len(shapes[idx]) != 0 else (chunk_len,)
+                
+                self.dset = h5f.create_dataset(
+                    i,
+                    shape=shape,
+                    maxshape=initial_maxshape, 
+                    dtype=dtype_,
+                    compression=compression,
+                    chunks=initial_chunkshape
+                )
+
+    @contextmanager
+    def open_hdf5_file(self, mode='a'):
+        file = h5py.File(self.datapath, mode)
+        try:
+            yield file
+        finally:
+            file.close()
+
+    def append(self, dataset, values, shape):
+        with self.open_hdf5_file('a') as h5f:
+            dset = h5f[dataset]
+            new_size = (self.i[dataset] + 1,) + shape if len(shape) != 0 else (self.i[dataset] + 1,)
+            dset.resize(new_size)
+            if isinstance(values, str):
+                values = np.array([values], dtype=h5py.string_dtype(encoding='utf-8'))
+            dset[self.i[dataset]] = values
+            self.i[dataset] += 1
+            h5f.flush()
 
 class VindrH5Converter(BaseH5Converter):
-    def __init__(self, data_dir: str, output_dir: str, chunk_size: int = 1000, num_processes: int = 2, split='training'):
+    def __init__(self, data_dir: str, output_dir: str, img_size: int = 224, chunk_size: int = 1000, num_processes: int = 2, split='training'):
         super().__init__(data_dir, output_dir, chunk_size, num_processes)
+        
+        self.img_size = img_size
         self.df_loader = VindrDataframeLoader(data_dir)
+    
+    def _init_df(self, split: str):
         self.df = self.df_loader(split=split)
         self.df.set_index('absolute_path', inplace=True)
-        self.output_dir = os.path.join(output_dir, split)
+        self.output_dir = os.path.join(self.output_dir, split)
         os.makedirs(self.output_dir, exist_ok=True)
 
         self.birads_dict = self.df['breast_birads'].to_dict()
         self.lesions_dict = self.df['finding_categories'].to_dict()
-
-    def _read_chunk(self, executor: ProcessPoolExecutor, paths: List[str]) -> Tuple[List[str], List[np.ndarray]]:
-        results = list(tqdm(executor.map(process_dicom_image, paths),
-                            total=len(paths), desc="Reading DICOMs", leave=False))
-
-        results = [r for r in results if r is not None]
-        if not results:
-            return [], []
-
-        processed_paths, pixel_arrays = zip(*results)
-        return list(processed_paths), list(pixel_arrays)
     
-    def convert(self):
+    def _process_dicom_image(self, path: str) -> Optional[Tuple[str, np.ndarray]]:
+        try:
+            image = resize_square(normalize_int8(read_dicom(path)), new_size=self.img_size)
+            return path, image
+        except Exception as e:
+            logging.warning(f"Failed to process {path}: {e}")
+            return None
+
+    def convert(self, split: str = 'training'):
+        
+        self._init_df(split)
         all_dicom_paths = self.df.index.tolist()
         total_files = len(all_dicom_paths)
         num_hdf5_files = ceil(total_files / self.chunk_size)
@@ -56,32 +97,33 @@ class VindrH5Converter(BaseH5Converter):
 
         start_index = 0
 
-        with ProcessPoolExecutor(max_workers=self.num_processes) as executor:
-            for i in trange(num_hdf5_files, desc="Converting chunks", unit="chunk"):
-                end_index = min(start_index + self.chunk_size, total_files)
-                chunk_paths = all_dicom_paths[start_index:end_index]
-                hdf5_filename = os.path.join(self.output_dir, f"chunk_{i:04d}.h5")
+        for i in trange(num_hdf5_files, desc="Converting chunks", unit="chunk"):
+            end_index = min(start_index + self.chunk_size, total_files)
+            chunk_paths = all_dicom_paths[start_index:end_index]
+            hdf5_filename = os.path.join(self.output_dir, f"chunk_{i:04d}.h5")
 
-                processed_paths, pixel_arrays = self._read_chunk(executor, chunk_paths)
+            hdf5_store = HDF5Store(
+                datapath=hdf5_filename,
+                datasets=["images", "image_paths", "birads_labels", "lesions_labels"],
+                shapes=[(self.img_size, self.img_size), (), (), ()],
+                dtype=[np.uint8, h5py.string_dtype(encoding='utf-8'), np.uint8, np.uint8]
+            )
 
-                if not processed_paths:
-                    logging.info("No valid DICOM images found in this chunk.")
-                    start_index = end_index
-                    continue
+            with ProcessPoolExecutor(max_workers=self.num_processes) as executor:
+                for result in tqdm(executor.map(self._process_dicom_image, chunk_paths), total=len(chunk_paths), desc="Processing DICOMs", leave=False):
+                    if result is None:
+                        continue
+                    path, image = result
+                    try:
+                        birads_label = self.birads_dict[path]
+                        lesion_label = self.lesions_dict[path]
+                    
+                        hdf5_store.append("images", image, image.shape)
+                        hdf5_store.append("image_paths", np.array(path, dtype=h5py.string_dtype(encoding='utf-8')), ())
+                        hdf5_store.append("birads_labels", np.array(birads_label, dtype=np.uint8), ())
+                        hdf5_store.append("lesions_labels", np.array(lesion_label, dtype=np.uint8), ())
+                    except Exception as e:
+                        logging.warning(f"Failed to process {path}: {e}")
 
-                birads = [self.birads_dict[path] for path in processed_paths]
-                lesions = [self.lesions_dict[path] for path in processed_paths]
-
-                try:
-                    with h5py.File(hdf5_filename, 'w') as h5f:
-                        h5f.create_dataset("images", data=np.stack(pixel_arrays), compression="lzf")
-                        h5f.create_dataset("paths", data=np.array(processed_paths, dtype='S'))
-                        birads_dataset = h5f.create_dataset("birads_labels", data=np.array(birads, dtype=np.int32))
-                        lesions_dataset = h5f.create_dataset("lesions_labels", data=np.array(lesions, dtype=np.int32))
-                        birads_dataset.attrs['label_mapping'] = json.dumps(self.df_loader.birads_mapping)
-                        lesions_dataset.attrs['label_mapping'] = json.dumps(self.df_loader.lesions_mapping)
-                    logging.info(f"HDF5 file saved: {hdf5_filename}")
-                except Exception as e:
-                    logging.info(f"Error writing HDF5 file {hdf5_filename}: {e}")
-
-                start_index = end_index
+            logging.info(f"HDF5 file saved: {hdf5_filename}")
+            start_index = end_index
